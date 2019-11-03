8416d1ec672634c0323ce8721e9c6642ac8fc4c8
Gil Mendes <gil00mendes@gmail.com>
2019-11-03 18:23:48 -0300

    create install script
    this install script will be extended to also install dependencies and
    all the required things for my daily work.
    
    Also, some files were moved to other folder to make all in the right
    place.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DOTFILES=$HOME/.dotfiles
+
+source ./install/common.sh
+source ./install/links.sh
+
+echo -e "\\nThanks for using my configs"
+echo -e "\\tGil Mendes<gil00mendes@gmail.com>"
+echo -e "\\thttps://github.com/gil0mendes/dotfiles"
