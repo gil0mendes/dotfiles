96cb11b985b71d69989b6ac1492de6cceb3814b7
Gil Mendes <gil00mendes@gmail.com>
2019-11-04 16:06:03 -0300

    add command to help reatach to tmux

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+command_exists() {
+  type "$1" > /dev/null 2>&1
+}
+
 DOTFILES=$HOME/.dotfiles
 
 source ./install/common.sh
 source ./install/links.sh
+
+if ! command_exists zsh; then
+  echo "zsh not found. Please install and then re-run installation scripts"
+  exit 1
+elif ! [[ $SHELL =~ .*zsh.* ]]; then
+  echo "Configuring zsh as default shell"
+  chsh -s "$(command -v zsh)"
+fi
 
 echo -e "\\nThanks for using my configs"
 echo -e "\\tGil Mendes<gil00mendes@gmail.com>"
