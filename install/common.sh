9e7984ab4cdf6453957b06368fb2055e0290ad51
Gil Mendes <gil00mendes@gmail.com>
2020-05-31 22:05:11 +0100

    apply some config updates (#1)
    * remove node
    
    * Update install.sh
    
    * installing nvm
    
    * Update zshrc.symlink
    
    * add iterm2
    
    * add rustup

--- install/common.sh
+++ install/common.sh
@@ -3,3 +3,6 @@ function print_header() {
   echo -e "$1"
   echo "========================================"
 } 
+
+print_header "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
