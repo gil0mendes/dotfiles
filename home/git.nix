bf4d6d4140991cfdace0d60ee5ac4342221aab47
Gil Mendes <gil00mendes@gmail.com>
2022-04-14 09:24:46 +0100

    Configure PGP key for personal computer

--- home/git.nix
+++ home/git.nix
@@ -31,9 +31,13 @@ in
     };
 
     # PGP signing for work
-    signing = mkIf isWork {
-      key = "3900180E4467EA40BB5CC04411EC8E1407151F84";
+    signing = {
       signByDefault = true;
-    };
+    } // (if isWork then {
+      key = "3900180E4467EA40BB5CC04411EC8E1407151F84";
+
+    } else {
+      key = "3C0FFA89D9EB7EF4BDABAB1B8108024DFE52031C";
+    });
   };
 }
